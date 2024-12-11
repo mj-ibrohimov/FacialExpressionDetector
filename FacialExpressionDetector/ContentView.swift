3281b2dd62a1f62608b98318e5e7493fcb7575e2
Muhammadjon_Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-12-12 00:25:13 +0100

    Initial Commit

---
+++ FacialExpressionDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FacialExpressionDetector
+//
+//  Created by Muhammadjon on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacialExpressionDetector/FacialExpressionDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialExpressionDetectorApp.swift
+//  FacialExpressionDetector
+//
+//  Created by Muhammadjon on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialExpressionDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
